4de50dd0ad98ff22ca08903a0ada945bb7360cac
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-10 14:43:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,7 @@ def initialize(board= nil)
   @board= board || array.new(9, " ")
 end
 
-def display_board(board)
+def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -131,4 +131,4 @@ end
 
 
 
-end 
+end
